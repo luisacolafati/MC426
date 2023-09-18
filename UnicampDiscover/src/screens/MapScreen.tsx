6ca912a4a1e36581c6a479b1cf00405ce31c1d18
Ana Helena Almeida <anahelenatla@gmail.com>
2023-09-17 21:27:56 -0300

    background with map only

--- UnicampDiscover/src/screens/MapScreen.tsx
+++ UnicampDiscover/src/screens/MapScreen.tsx
@@ -2,38 +2,30 @@ import { useState } from 'react';
 import * as React from 'react';
 import { View, Text, ImageBackground, StyleSheet, SafeAreaView } from 'react-native'; 
 import { styles } from '../styles/styles';
-import { SearchBar } from '@rneui/themed';
-import { Searchbar } from 'react-native-paper';
+//import { SearchBar } from '@rneui/themed';
+//import { Searchbar } from 'react-native-paper';
 
 
 const image = {uri: 'https://www.cle.unicamp.br/trendsxvi/images/sys/mapa.png'};
-// const [search, setSearch] = useState("");
-// type SearchBarComponentProps = {};
-//     const SwitchComponent: React.FunctionComponent<SearchBarComponentProps> = () => {
-//         const [search, setSearch] = useState("");
+
+// const MyComponent = () => {
+//     const [searchQuery, setSearchQuery] = React.useState('');
+  
+//     const onChangeSearch = (query: React.SetStateAction<string>) => setSearchQuery(query);
+  
+//     return (
+//       <Searchbar
+//         iconColor = "#850a0a"
+//         placeholder="Search"
+//         placeholderTextColor={'#850a0a'}
+//         onChangeText={onChangeSearch}
+//         value={searchQuery}
         
-//         const updateSearch = (search: React.SetStateAction<string>) => {
-//           setSearch(search);
-//         };
-//     }
-const MyComponent = () => {
-    const [searchQuery, setSearchQuery] = React.useState('');
+//       />
+//     );
+//   };
   
-    const onChangeSearch = (query: React.SetStateAction<string>) => setSearchQuery(query);
-  
-    return (
-      <Searchbar
-        iconColor = "#850a0a"
-        placeholder="Search"
-        placeholderTextColor={'#850a0a'}
-        onChangeText={onChangeSearch}
-        value={searchQuery}
-        
-      />
-    );
-  };
-  
-  export default MyComponent;
+//   export default MyComponent;
 export function MapScreen( ){
     
     
@@ -42,7 +34,7 @@ export function MapScreen( ){
             
             <ImageBackground source={image} resizeMode="cover" style={styles.imageBackground}>
                
-                <MyComponent></MyComponent>
+                {/* <MyComponent></MyComponent> */}
             </ImageBackground>
             
         </SafeAreaView>
