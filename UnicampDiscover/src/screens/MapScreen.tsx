44adac92ccd58a4500dcdb7ed0720f1352771785
gmedranos <g247113@dac.unicamp.br>
2023-12-08 19:11:49 -0300

    Adiciona o mapa que foca na posiçao do usuario

--- UnicampDiscover/src/screens/MapScreen.tsx
+++ UnicampDiscover/src/screens/MapScreen.tsx
@@ -1,49 +1,94 @@
-import { useState } from 'react'; 
-import * as React from 'react';
-import { View, Text, ImageBackground, StyleSheet, SafeAreaView } from 'react-native'; 
-import { styles } from '../styles/styles';
-import { SearchBar } from '@rneui/themed';
-import { Searchbar } from 'react-native-paper';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import {requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject, watchHeadingAsync, watchPositionAsync, LocationAccuracy} from 'expo-location'
 
 
-const image = {uri: 'https://www.cle.unicamp.br/trendsxvi/images/sys/mapa.png'};
-// const [search, setSearch] = useState("");
-// type SearchBarComponentProps = {};
-//     const SwitchComponent: React.FunctionComponent<SearchBarComponentProps> = () => {
-//         const [search, setSearch] = useState("");
-        
-//         const updateSearch = (search: React.SetStateAction<string>) => {
-//           setSearch(search);
-//         };
-//     }
-const MyComponent = () => {
-    const [searchQuery, setSearchQuery] = React.useState('');
-  
-    const onChangeSearch = (query: React.SetStateAction<string>) => setSearchQuery(query);
-  
-    /*return (
-      <Searchbar
-        iconColor = "#850a0a"
-        placeholder="Search"
-        placeholderTextColor={'#850a0a'}
-        onChangeText={onChangeSearch}
-        value={searchQuery}
-        
-      />
-    );*/
+
+export function MapScreen() {
+
+  const initialRegion = {
+    latitude: -22.8135,
+    longitude: -47.0635,
+    latitudeDelta: 0.001,
+    longitudeDelta: 0.001,
   };
+
+  const [location, setLocation] = useState<LocationObject | null>(null);
+  const [mapRegion, setMapRegion] = useState(initialRegion);
+
+  const markerCoordinates = {
+    latitude: -22.8135,
+    longitude: -47.0635,
+  };
+
+  async function requestLocation() {
+    const {granted} = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const posicao = await getCurrentPositionAsync();
+      setLocation(posicao);
+    }
+  }
+
+  useEffect(() => {
+    requestLocation();
+  }, []);
+
+  useEffect(() => {
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval: 1000,
+      distanceInterval: 1
+    }, (response) => {
+      setLocation(response);
+      setMapRegion({
+        latitude: response.coords.latitude,
+        longitude: response.coords.longitude,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001
+      })
+      console.log(response);
+    });
+  }, [])
+
   
-  export default MyComponent;
-export function MapScreen( ){
-    
-    
-    return (
-        <SafeAreaView style={styles.container}>
-            
-            <ImageBackground source={image} resizeMode="cover" style={styles.imageBackground}>
-            </ImageBackground>
-        </SafeAreaView>
-        
-        
-    );
+return (
+<SafeAreaView style={styles.container}>
+<MapView
+style={styles.map}
+customMapStyle={mapstyle}
+initialRegion={ initialRegion}
+region = {mapRegion}
+>
+<Marker
+coordinate={{
+latitude: location?.coords.latitude || 0,
+longitude: location?.coords.longitude || 0
+}}
+/>
+</MapView>
+</SafeAreaView>
+);
 }
+
+const mapstyle = 
+[
+  {
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  }
+]
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
