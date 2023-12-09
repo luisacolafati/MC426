fcefc365d918ddb3489f4a8eb919c264c03c3bce
gmedranos <g247113@dac.unicamp.br>
2023-12-09 14:25:36 -0300

    Adiciona uma logica para adicionar os institutos a partir de uma lista

--- UnicampDiscover/src/screens/MapScreen.tsx
+++ UnicampDiscover/src/screens/MapScreen.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import {requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject, watchHeadingAsync, watchPositionAsync, LocationAccuracy} from 'expo-location'
+import { Section } from 'react-native-paper/lib/typescript/src/components/List/List';
 
 
 
@@ -16,11 +17,13 @@ export function MapScreen() {
 
   const [location, setLocation] = useState<LocationObject | null>(null);
   const [mapRegion, setMapRegion] = useState(initialRegion);
+  const [institutes, setInstitutes] = useState<any[]>([])
 
   const markerCoordinates = {
     latitude: -22.8135,
     longitude: -47.0635,
   };
+
 
   async function requestLocation() {
     const {granted} = await requestForegroundPermissionsAsync();
@@ -31,10 +34,12 @@ export function MapScreen() {
     }
   }
 
+  // Pede a localizaçao
   useEffect(() => {
     requestLocation();
   }, []);
 
+  // Observa a posicao do dispositivo
   useEffect(() => {
     watchPositionAsync({
       accuracy: LocationAccuracy.Highest,
@@ -42,32 +47,75 @@ export function MapScreen() {
       distanceInterval: 1
     }, (response) => {
       setLocation(response);
-      setMapRegion({
-        latitude: response.coords.latitude,
-        longitude: response.coords.longitude,
-        latitudeDelta: 0.001,
-        longitudeDelta: 0.001
-      })
-      console.log(response);
+
+      /*Essa parte comentada a baixo é responsavel por focar no usuario
+      Minha ideia é ter um botao q ativa/desativa isso, que nem no waze
+      porque se isso estiver sempre ligado o usuario nao consegue olhar 
+      o mapa
+      */
+
+      //setMapRegion({
+      //  latitude: response.coords.latitude,
+      //  longitude: response.coords.longitude,
+      //  latitudeDelta: 0.001,
+      //  longitudeDelta: 0.001
+      //})
     });
   }, [])
 
+  useEffect(() =>  {
+    // Essa lista deveria vir do banco de dados, mas nn sei fazer isso agora
+    // Então vou usar essa lista manual e depois penso no FireBase
+    // Mas a logica pra adicionar os marcadores ta ai
+    const listInstitutes = [{
+      id: 1,
+      nome: "IC3",
+      latitude: -22.81362,
+      longitude: -47.06391
+    }, {
+      id:2,
+      nome:"IE",
+      latitude: -22.81493,
+      longitude: -47.0658
+    },
+    {
+      id:3,
+      nome:"IMECC",
+      latitude: -22.81576,
+      longitude: -47.06779
+    }
+  ]
+
+      setInstitutes(listInstitutes);
+  }, []);
   
 return (
 <SafeAreaView style={styles.container}>
-<MapView
-style={styles.map}
-customMapStyle={mapstyle}
-initialRegion={ initialRegion}
-region = {mapRegion}
->
-<Marker
-coordinate={{
-latitude: location?.coords.latitude || 0,
-longitude: location?.coords.longitude || 0
-}}
-/>
-</MapView>
+  <MapView
+  style={styles.map}
+  customMapStyle={mapstyle}
+  initialRegion={ initialRegion}
+  region = {mapRegion}
+  >
+    <Marker
+    coordinate={{
+    latitude: location?.coords.latitude || 0,
+    longitude: location?.coords.longitude || 0
+    }}
+    />
+
+    {institutes.map((institute) => (
+        <Marker
+          key={institute.id}
+          coordinate={{
+            latitude: institute.latitude,
+            longitude: institute.longitude,
+          }}
+          title={institute.nome}
+        />
+      ))}
+    
+  </MapView>
 </SafeAreaView>
 );
 }
