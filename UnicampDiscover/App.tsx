9bb53dcc6e45a674112544704a521c462bcb7c78
Luisa Colafati <luisacolafati@gmail.com>
2023-12-10 10:09:02 -0300

    remove test code from App.ts

--- UnicampDiscover/App.tsx
+++ UnicampDiscover/App.tsx
@@ -1,30 +1,10 @@
 import { TabRoutes } from './src/routes/tab.routes';
 import { NavigationContainer } from '@react-navigation/native';
-import { useEffect, useState } from 'react';
-import { CSVFilePickerButton } from './src/components/CSVFilePickerButton';
-import { FileService } from './src/services/files/FileService';
-import { FirestoreService } from './src/services/firestore/FirestoreService';
 
 export default function App() {
-  const [fileUri, setFileUri] = useState('')
-  const fileService = new FileService()
-
-  // useEffect for test
-  useEffect(() => {
-    const firestoreService = new FirestoreService('bathrooms')
-    firestoreService.addDocument({ id: '', data: { instituteLocation: "IG" } })
-  }, [])
-
-  useEffect(() => {
-    fileService.readCSVFileFromUri(fileUri)
-  }, [fileUri])
-
   return (
     <NavigationContainer>
       <TabRoutes />
-      <CSVFilePickerButton
-        setFileUri={setFileUri}
-      />
     </NavigationContainer>
   );
 }
