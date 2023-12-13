c1d3097689330559f523fdef0408ac9955e338de
anahelenatla <anahelenatla@gmail.com>
2023-12-13 00:11:24 -0300

    tentando mergear

---
+++ UnicampDiscover/src/types/Bathroom.ts
@@ -0,0 +1,15 @@
+import { Gender } from "../enums/GenderEnum"
+import { Institutes } from "../enums/InstitutesEnum"
+import { Rating } from "./Rating"
+
+export type Bathroom = {
+    id: string
+    data: {
+        floor: number
+        gender: Gender
+        isAccessible: boolean
+        instituteLocation: Institutes
+        avaliacao: number
+    }
+    rating: Rating
+}
