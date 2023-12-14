4d279f0b84cc56bf31c21e7dd2359bfbdb90e346
Luisa Colafati <luisacolafati@gmail.com>
2023-12-14 03:03:24 -0300

    adding mocked data to minimize data base access

--- UnicampDiscover/src/database/CollectionNames.ts
+++ UnicampDiscover/src/database/CollectionNames.ts
@@ -1,4 +1,5 @@
 export enum CollectionNames {
     BATHROOMS = 'bathrooms',
-    DRINKING_FOUNTAIN = 'drinkingFountain'
+    DRINKING_FOUNTAIN = 'drinking_fountain',
+    INSTITUTES = 'institutes'
 }
